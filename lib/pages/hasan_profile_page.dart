6449bc189668c6f3c62552f7e6a2cff8e5e18b49
hasan948-spec <hanzhasan948@sar.ac.id>
2026-05-25 09:31:54 +0700

    pardi hardcore conflict X, kapmesan tok

--- lib/pages/hasan_profile_page.dart
+++ lib/pages/hasan_profile_page.dart
@@ -11,6 +11,7 @@ class _HasanProfilePageState extends State<HasanProfilePage> {
   final TextEditingController _unptUsrnm = TextEditingController();
   final TextEditingController _eml = TextEditingController();
   final TextEditingController _web = TextEditingController();
+  final TextEditingController _no = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,6 +41,7 @@ class _HasanProfilePageState extends State<HasanProfilePage> {
               SizedBox(height: 15),
               Icon(Icons.person_2, color: Colors.white, size: 200),
               SizedBox(height: 15),
+              Text("Edit Profile",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 20),),
               SizedBox(
                 height: 100,
                 width: 400,
@@ -47,9 +49,97 @@ class _HasanProfilePageState extends State<HasanProfilePage> {
                   controller: _unptUsrnm,
                   keyboardType: TextInputType.text,
                   decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
                     border: OutlineInputBorder(),
                     labelText: "Name",
                     labelStyle: TextStyle(color: Colors.white),
+                    focusColor: Colors.white,
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              SizedBox(
+                height: 100,
+                width: 400,
+                child: TextField(
+                  controller: _eml,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    border: OutlineInputBorder(),
+                    labelText: "Email baru",
+                    labelStyle: TextStyle(color: Colors.white),
+                    focusColor: Colors.white,
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              SizedBox(
+                height: 100,
+                width: 400,
+                child: TextField(
+                  controller: _web,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    border: OutlineInputBorder(),
+                    labelText: "web",
+                    labelStyle: TextStyle(color: Colors.white),
+                    focusColor: Colors.white,
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              Text("Personal Details",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 20),),
+              SizedBox(height: 20),
+              DropdownMenu(
+                inputDecorationTheme: InputDecorationTheme(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
+                width: 400,
+                label: Text("Pilih Gender",style: TextStyle(color: Colors.white),),
+                textStyle: TextStyle(color: Colors.white),
+                dropdownMenuEntries: [
+                  DropdownMenuEntry(value: "Male", label: "Male"),
+                  DropdownMenuEntry(value: "Female", label: "Female"),
+                  DropdownMenuEntry(value: "Other", label: "Other"),
+                ],
+              ),
+              SizedBox(height: 50),
+              SizedBox(
+                height: 100,
+                width: 400,
+                child: TextField(
+                  controller: _no,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    border: OutlineInputBorder(),
+                    labelText: "Nomer hp",
+                    labelStyle: TextStyle(color: Colors.white),
+                    focusColor: Colors.white,
                     fillColor: Colors.white,
                     focusedBorder: OutlineInputBorder(
                       borderSide: BorderSide(color: Colors.white),
